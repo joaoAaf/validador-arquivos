e928470d2e19fa462acf776711136c619549f575
João Anderson <joao.anderson.af@gmail.com>
2026-02-28 17:33:41 -0300

    Refatorando testes

--- test/ValidadorArquivo.js
+++ test/ValidadorArquivo.js
@@ -1,16 +1,23 @@
-import { describe, it } from 'node:test'
+import { describe, it, beforeEach } from 'node:test'
 import { expect } from 'chai'
 import { network } from 'hardhat'
 import { ethers as ethLib } from 'ethers'
 
 describe("ValidadorArquivo", function () {
-    it("Deve registrar e validar um hash de um arquivo corretamente", async function () {
-        const { ethers } = await network.connect()
+    let contrato
+    let ethers
+
+    beforeEach(async function () {
+        // Conexão com a rede local do Hardhat
+        const connection = await network.connect()
+        ethers = connection.ethers
 
         // Deploy do contrato
         const ValidadorArquivo = await ethers.getContractFactory("ValidadorArquivo")
-        const contrato = await ValidadorArquivo.deploy()
+        contrato = await ValidadorArquivo.deploy()
+    })
 
+    it("Deve registrar e validar um hash de um arquivo corretamente", async function () {
         // Criação de um hash de teste para o arquivo
         const testeHash = ethLib.keccak256(ethLib.toUtf8Bytes("arquivo_de_teste"))
 
@@ -27,13 +34,8 @@ describe("ValidadorArquivo", function () {
     })
 
     it("Deve retornar invalido para arquivo inexistente", async function () {
-        const { ethers } = await network.connect()
-        
-        const ValidadorArquivo = await ethers.getContractFactory("ValidadorArquivo");
-        const contract = await ValidadorArquivo.deploy();
-
-        const hashFalso = ethLib.keccak256(ethLib.toUtf8Bytes("arquivo_falso"));
-        const resultado = await contract.validarArquivo(hashFalso);
+        const hashFalso = ethLib.keccak256(ethLib.toUtf8Bytes("arquivo_falso"))
+        const resultado = await contrato.validarArquivo(hashFalso)
 
         expect(resultado.ehValido).to.equal(false)
     })
